4889a8b743bf3b72f45b314d577ea72eafcb65c0
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-05-28 18:32:31 +0700

    LAB1.4 second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     const url = req.url;
     if (url === '/') {
-        res.write('<html><body><form><input type="text" name="message" /><button type="submid">Send</button></form></body></html>');
+        res.write('<html><body><form action="/message" method="POST"><input type="text" name="message" /><button type="submid">Send</button></form></body></html>');
         return res.end();
     }
     res.setHeader('Content-Type','text/html');
