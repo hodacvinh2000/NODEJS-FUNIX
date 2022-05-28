a3dc95d677efb06fa4e57bf6bed576562677b17d
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-05-28 17:00:32 +0700

    LAB1.4

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html><body><form><input type="text" name="message" /><button type="submid">Send</button></form></body></html>');
+        return res.end();
+    }
     res.setHeader('Content-Type','text/html');
     res.write('<html><body></body><h1>Hello from my Node.js server!</h1></html>');
     res.end();
