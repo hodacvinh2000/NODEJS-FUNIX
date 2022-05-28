a62e5ec3009093baf66d47c6bfefd5e139e28541
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-05-28 14:43:59 +0700

    LAB1.3

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req);
+
+    res.setHeader('Content-Type','text/html');
+    res.write('<html><body></body><h1>Hello from my Node.js server!</h1></html>');
+    res.end();
 });
 
 server.listen(3000);
