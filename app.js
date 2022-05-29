3543d6b01b44b8a4e1dead6bcf05f56bbeda1c7d
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-05-29 09:07:10 +0700

    lab1.7

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write('<html><body><form action="/message" method="POST"><input type="text" name="message" /><button type="submid">Send</button></form></body></html>');
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-            console.log(chunk);
-        });
-        req.on('end', () => {
-            const parseBody  = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            fs.writeFileSync('message.txt',message);
-            console.log(parseBody);
-            res.statusCode = 302;
-            res.setHeader('Location','/');
-            return res.end();
-        });
-    }
-    res.setHeader('Content-Type','text/html');
-    res.write('<html><body></body><h1>Hello from my Node.js server!</h1></html>');
-    res.end();
-});
+console.log(routes.sometext);
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
