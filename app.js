54b4bb48680e254ec147c3b3553afa71222128e6
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-06-02 06:35:50 +0700

    lab2.7

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use("/add-product", (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Send</button></form>')
-});
-
-app.post("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use("/", (req, res, next) => {
-    res.send('<h1>Hello from Express.js</h1>')
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
