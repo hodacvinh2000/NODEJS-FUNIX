a004a895a231471030929cad21e158e7fa3dc80b
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-06-05 21:59:34 +0700

    install and use pug

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const rootDir = require('./util/path');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')));
 
