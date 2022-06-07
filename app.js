3c842dfe58129f8bf9f0b9b93cf9617b4a1606f4
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-06-07 20:27:46 +0700

    convert to handlebars template

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
