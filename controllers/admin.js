ed2b9c1bead8f5886bc92fa15f1fc49f23c6713c
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-06-12 16:03:51 +0700

    inserting data into the database

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,7 +15,14 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
+  product
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .cath((err) => {
+      console.log(err);
+    });
   res.redirect("/");
 };
 
