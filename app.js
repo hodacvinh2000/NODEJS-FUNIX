29a88ceb340cac2b9d0aaf8acdd83f90ebbcf41d
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-06-16 21:33:57 +0700

    Using the Database Connection

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoConnect = require("./util/database");
+const mongoConnect = require("./util/database").mongoConnect;
 
 const adminRoutes = require("./routes/admin");
 // const shopRoutes = require("./routes/shop");
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
   //     next();
   //   })
   //   .catch((err) => console.log(err));
+  next();
 });
 
 app.use("/admin", adminRoutes);
