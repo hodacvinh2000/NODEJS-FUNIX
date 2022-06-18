b92a92fe727fc2032dac82f4e9a3b41f1d708d60
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-06-18 22:43:29 +0700

    Adding the "Add to Cart" Functionality

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("62ade5b960b6393508807284")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
--- models/user.js
+++ models/user.js
@@ -18,7 +18,9 @@ class User {
     // const cartProduct = this.cart.findIndex(cp => {
     //   return cp._id === product._id;
     // });
-    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const updatedCart = {
+      items: [{ productId: new ObjectId(product._id), quantity: 1 }],
+    };
     const db = getDb();
     return db
       .collection("users")
