6ec76d202ffc349577bdeac700ece2999e5b7c44
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-06-19 09:53:28 +0700

    Fixing bug

--- controllers/shop.js
+++ controllers/shop.js
@@ -61,6 +61,7 @@ exports.postCart = (req, res, next) => {
       return req.user.addToCart(product);
     })
     .then((result) => {
+      res.redirect("/cart");
       console.log(result);
     });
   // let fetchedCart;
