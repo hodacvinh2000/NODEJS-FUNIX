4fc98feb33618c40a189e7bdcc2a3c55c60ab856
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-06-19 21:02:29 +0700

    One Important Thing About Fetching Relations

--- controllers/admin.js
+++ controllers/admin.js
@@ -80,7 +80,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    .select("title price -_id") // -_id if you don't want to get _id
+    .populate("userId", "name")
     .then((products) => {
+      console.log(products);
       res.render("admin/products", {
         prods: products,
         pageTitle: "Admin Products",
