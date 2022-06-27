6cab8d11c17d671121f677eabd4183268648268e
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-06-27 21:51:41 +0700

    Using the Session Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,6 +15,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
