897e9d97ec97a50768b2d9f1366f32d75377611e
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-06-27 21:02:33 +0700

    Setting a Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,19 @@
 exports.getLogin = (req, res, next) => {
+  const cookieArray = req
+    .get("Cookie")
+    .split(";")
+    .map((c) => c.trim().split("="));
+  const cookieObject = {};
+  cookieArray.map((c) => (cookieObject[c[0]] = c[1]));
   res.render("auth/login", {
     path: "/login",
     pageTitle: "login",
-    isAuthenticated: req.isLoggedIn,
+    isAuthenticated: cookieObject.loggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.setHeader("Set-Cookie", "abc=1");
   res.redirect("/");
 };
