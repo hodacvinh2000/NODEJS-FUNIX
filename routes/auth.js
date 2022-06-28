ec461e36008e80fd38b3be9a6d0491ac6188f299
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-06-28 22:45:51 +0700

    Adding Async Validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { check, body } = require("express-validator/check");
+const User = require("../models/user");
 
 const authController = require("../controllers/auth");
 
@@ -17,11 +18,18 @@ router.post(
     check("email")
       .isEmail()
       .withMessage("Please enter a valid email")
-      .custom((value) => {
-        if (value === "test@test.com") {
-          throw new Error("This email address if forbidden");
-        }
-        return true;
+      .custom((value, { req }) => {
+        // if (value === "test@test.com") {
+        //   throw new Error("This email address if forbidden");
+        // }
+        // return true;
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              "Email exists already, please pick a different one."
+            );
+          }
+        });
       }),
     body(
       "password",
