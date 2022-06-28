706e7b09173b32bb878835884431b3c78485d201
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-06-28 22:08:01 +0700

    Setup & Basic Validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator/check");
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -63,6 +64,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array().find((e) => e.param === "email").msg,
+    });
+  }
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
