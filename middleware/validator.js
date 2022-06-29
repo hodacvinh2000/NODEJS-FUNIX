062d9b209ff7da815358b920b1ceb5c9f0aedd1b
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-06-29 20:41:58 +0700

    Sanitizing Data

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,15 +1,12 @@
 const { check, body } = require("express-validator/check");
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 const checkSignupForm = [
   check("email")
     .isEmail()
     .withMessage("Please enter a valid email")
-    .custom((value, { req }) => {
-      // if (value === "test@test.com") {
-      //   throw new Error("This email address if forbidden");
-      // }
-      // return true;
+    .custom((value) => {
       return User.findOne({ email: value }).then((userDoc) => {
         if (userDoc) {
           return Promise.reject(
@@ -17,45 +14,47 @@ const checkSignupForm = [
           );
         }
       });
-    }),
+    })
+    .trim(),
   body(
     "password",
     "Please enter a password with only numbers and text and at least 5 charaters."
   )
     .isLength({ min: 5 })
-    .isAlphanumeric(),
-  body("confirmPassword").custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw Error("Passwords have to match!");
-    }
-    return true;
-  }),
+    // .isAlphanumeric()
+    .trim(),
+  body("confirmPassword")
+    .trim()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw Error("Passwords have to match!");
+      }
+      return true;
+    }),
 ];
 
 const checkLoginForm = [
   check("email")
     .isEmail()
     .withMessage("Please enter a valid email")
-    .custom((value, { req }) => {
-      // if (value === "test@test.com") {
-      //   throw new Error("This email address if forbidden");
-      // }
-      // return true;
+    .normalizeEmail()
+    .custom((value) => {
       return User.findOne({ email: value }).then((userDoc) => {
         if (!userDoc) {
           return Promise.reject("Email do not exists.");
         }
       });
     }),
-  check("password").custom((value, { req }) => {
-    return User.findOne({ email: req.body.email, password: value }).then(
-      (userDoc) => {
-        if (!userDoc) {
+  check("password")
+    .trim()
+    .custom((value, { req }) => {
+      return User.findOne({ email: req.body.email }).then(async (userDoc) => {
+        const isRightPassword = await bcrypt.compare(value, userDoc.password);
+        if (!isRightPassword) {
           return Promise.reject("Wrong password.");
         }
-      }
-    );
-  }),
+      });
+    }),
 ];
 
 exports.checkLoginForm = checkLoginForm;
